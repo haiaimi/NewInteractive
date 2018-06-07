128b3eb9f0430b27307185899ce9ab5e12789106
haiaimi <wanghai1997@outlook.com>
2018-06-07 18:16:50 +0800

    通用工具类

---
+++ TestProject/Private/Common/TestProjectHelper.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestProjectHelper.h"
+#include "Engine/Engine.h"
+#include "TestProject/TestProject.h"
+#include "MainController.h"
+#include "SceneView.h"
+#include "Engine/LocalPlayer.h"
+
+
+void TestProjectHelper::Debug_ScreenMessage(FString&& InString, FColor FontColor)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FontColor, InString);
+}
+
+void TestProjectHelper::Debug_LogMessage(FString&& InString)
+{
+	UE_LOG(LogTest, Log, TEXT("%s"), *InString)
+}
+
+void TestProjectHelper::DeProjectScreenToWorld(const AMainController* PlayerControl, FVector& OutWorldPos, FVector& OutWorldDir)
+{
+	FVector2D MousePosition;
+	PlayerControl->GetMousePosition(MousePosition.X, MousePosition.Y);    //获取鼠标位置
+
+	FSceneViewProjectionData ProjectionData;     //投影数据
+	FViewport* ViewPort = PlayerControl->GetLocalPlayer()->ViewportClient->Viewport;
+	PlayerControl->GetLocalPlayer()->GetProjectionData(ViewPort, eSSP_FULL, ProjectionData);      //获取当前的投影数据
+
+	FMatrix const InvViewProjMatrix = ProjectionData.ComputeViewProjectionMatrix().InverseFast();     //获取投影矩阵的逆，这样就可以根据NDC空间得出在世界坐标系中的位置
+	FSceneView::DeprojectScreenToWorld(MousePosition, ProjectionData.GetConstrainedViewRect(), InvViewProjMatrix, OutWorldPos, OutWorldDir);   //其流程就是: 屏幕坐标-->NDC坐标-->世界坐标
+}
---
+++ TestProject/Public/Common/TestProjectHelper.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainController.h"
+
+/**
+ * 该项目通用工具类，主要是静态成员，如屏幕输出和日志
+ */
+
+class TESTPROJECT_API TestProjectHelper
+{
+public:
+	static void Debug_ScreenMessage(FString&& InString, FColor FontColor = FColor::Blue);
+
+	static void Debug_LogMessage(FString&& InString);
+
+	/**根据屏幕空间计算其所在的世界空间*/
+	static void DeProjectScreenToWorld(const AMainController* PlayerContorl, FVector& OutWorldPos, FVector& OutWorldDir);
+};
