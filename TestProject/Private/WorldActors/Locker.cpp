83b9ced1964ee60112db0a1d7824159f4c0bbd82
haiaimi <wanghai1997@outlook.com>
2018-06-08 18:24:49 +0800

    添加自定义碰撞通道

--- TestProject/Private/WorldActors/Locker.cpp
+++ TestProject/Private/WorldActors/Locker.cpp
@@ -5,6 +5,8 @@
 #include "ConstructorHelpers.h"
 #include "Common/TestProjectHelper.h"
 #include "Components/SpotLightComponent.h"
+#include "InventoryActor.h"
+#include "TestProject.h"
 
 
 // Sets default values
@@ -19,7 +21,7 @@ ALocker::ALocker()
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshFinder(TEXT("/Game/StarterContent/Architecture/Floor_400x400"));
 	static ConstructorHelpers::FObjectFinder<UMaterialInterface> MaterialFinder(TEXT("/Game/StarterContent/Materials/M_Wood_Oak"));
-	if (!ensure(MeshFinder.Succeeded()))return;
+	if (!ensure(MeshFinder.Succeeded()))return;	
 	if (!ensure(MaterialFinder.Succeeded()))return;
 
 	LockerMesh = MeshFinder.Object;
@@ -27,6 +29,9 @@ ALocker::ALocker()
 
 	if (LockerMeshComponent && BaseScene)
 	{
+		LockerMeshComponent->SetCollisionResponseToChannel(COLLISION_LOCKERRAY, ECollisionResponse::ECR_Block);
+		LockerMeshComponent->SetCollisionResponseToChannel(COLLISION_INVENTORYRAY, ECollisionResponse::ECR_Ignore);
+		
 		RootComponent = BaseScene;
 		LockerMeshComponent->SetupAttachment(BaseScene);
 		LockerMeshComponent->SetStaticMesh(LockerMesh);
@@ -34,7 +39,22 @@ ALocker::ALocker()
 		FVector LockerBound = LockerMeshComponent->Bounds.BoxExtent;
 		LockerMeshComponent->SetRelativeLocation(FVector(-LockerBound.X, -LockerBound.Y, 0.f));
 		SetActorScale3D(FVector(0.4f, 1.5f, 1.f));
+		LockerLength = LockerBound.Y * 2.f * 1.5f;
+		LockerWidth = LockerBound.X * 2.f * 0.4f;
+
+		if (SpotLight)
+		{
+			SpotLight->Mobility = EComponentMobility::Type::Movable;
+			SpotLight->SetupAttachment(BaseScene);
+			SpotLight->SetRelativeRotation(FRotator(-90.f, 0.f, 0.f));
+			SpotLight->IntensityUnits = ELightUnits::Candelas;
+			SpotLight->Intensity = 160.f;
+			SpotLight->OuterConeAngle = 24.f;
+		}
 	}
+
+	LockerCapacity = 4;    //默认储物柜的容量为4
+	TestProjectHelper::Debug_ScreenMessage(FString::Printf(TEXT("Locker Length: %f, Locker Width: %f"), LockerLength, LockerWidth));
 }
 
 // Called when the game starts or when spawned
@@ -42,6 +62,7 @@ void ALocker::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	LockerContent.SetNum(4);      //设置储物柜的容量
 	TestProjectHelper::Debug_ScreenMessage(RootComponent->GetName());
 }
 
@@ -52,3 +73,78 @@ void ALocker::Tick(float DeltaTime)
 
 }
 
+void ALocker::AddInventoryThing(class AInventoryActor* AddedActor, FVector CursorLastPoint)
+{
+	FVector LockerCenter = GetActorLocation();    //获取储物柜中心位置
+	const float CursorToLeftOffsetY = CursorLastPoint.Y - (LockerCenter.Y - LockerLength / 2);   //光标到储物柜中心Y方向的偏移
+	const float PerGridWidth = LockerLength / LockerCapacity;    //储物柜每个方格的宽度
+	int32 ArrayIndex = static_cast<int32>(CursorToLeftOffsetY / PerGridWidth);
+	bool bCanAddToLocker = false;
+
+	if (LockerContent[ArrayIndex])
+	{
+		for (int32 i = 0; i < LockerContent.Num(); ++i)
+		{
+			if (!LockerContent[i])
+			{
+				bCanAddToLocker = true;
+				LockerContent[i] = AddedActor;
+				ArrayIndex = i;
+				break;  //结束循环
+			}
+		}
+	}
+	else
+	{
+		LockerContent[ArrayIndex] = AddedActor;
+		bCanAddToLocker = true;
+	}
+
+	if (bCanAddToLocker)
+	{
+		AddedActor->bInLocker = true;       //该物体已在储物柜中
+		AddedActor->AttachToActor(this, FAttachmentTransformRules::KeepWorldTransform);   //把仓库物体附着在储物柜上
+		FVector DestLocation(LockerCenter.X, LockerCenter.Y - LockerLength / 2 + ArrayIndex * PerGridWidth + PerGridWidth / 2, LockerCenter.Z + AddedActor->GetHeight() / 2);
+		//AddedActor->SetActorLocation(DestLocation);  //设置仓库物体的具体位置
+		AddedActor->bIsInMove = true;
+		AddedActor->DestLocation = DestLocation;   //设置新的目标位置
+	}
+	else
+	{
+		AddedActor->bIsInMove = true;
+		AddedActor->DestLocation = AddedActor->OriginLocation;
+	}
+}
+
+void ALocker::RemoveInventoryThing(class AInventoryActor* RemovedActor)
+{
+	int32 RemoveIndex = LockerContent.Find(RemovedActor);
+	LockerContent[RemoveIndex] = nullptr;
+	TestProjectHelper::Debug_LogMessage(FString::FormatAsNumber(RemoveIndex));
+}
+
+void ALocker::CastLight(class AInventoryActor* CastedActor)
+{
+	//获取3个轴的方向
+	SpotLight->Intensity = 160.f;
+	SpotLight->UpdateComponentToWorld();
+	const FMatrix LockerRot = FRotationMatrix(GetActorRotation());
+	const FVector DirX = LockerRot.GetUnitAxis(EAxis::Type::X);
+	const FVector DirY = LockerRot.GetUnitAxis(EAxis::Type::Y);
+	const FVector DirZ = LockerRot.GetUnitAxis(EAxis::Type::Z);
+
+	int32 CastedActorIndex = LockerContent.Find(CastedActor);
+	const float PerGridWidth = 150.f;
+	float RelativeY = CastedActorIndex * PerGridWidth + 0.5f * PerGridWidth - (LockerLength * 0.5f);    //Y方向的相对距离
+
+	TestProjectHelper::Debug_ScreenMessage(FString::FormatAsNumber(CastedActorIndex));
+	FVector RelativeLoc = DirZ * 200.f + DirY.GetSafeNormal() * RelativeY * 0.7f;
+	SpotLight->SetRelativeLocation(RelativeLoc);
+	SpotLight->SetRelativeRotation(FRotator(-90.f, 0.f, 0.f));
+}
+
+void ALocker::StopCastLight()
+{
+	TestProjectHelper::Debug_ScreenMessage(TEXT("StopLight"));
+	SpotLight->Intensity = 0.f;
+}
--- TestProject/TestProject.h
+++ TestProject/TestProject.h
@@ -5,3 +5,6 @@
 #include "CoreMinimal.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogTest, Log, All);
+
+#define COLLISION_LOCKERRAY     ECollisionChannel::ECC_GameTraceChannel1
+#define COLLISION_INVENTORYRAY  ECollisionChannel::ECC_GameTraceChannel2
