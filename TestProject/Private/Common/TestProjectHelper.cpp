a04af5211d1384c9c8529c9c27948797ea39bf61
haiaimi <wanghai1997@outlook.com>
2018-06-23 17:59:08 +0800

    添加对屏幕指定位置的反投影

--- TestProject/Private/Common/TestProjectHelper.cpp
+++ TestProject/Private/Common/TestProjectHelper.cpp
@@ -18,7 +18,7 @@ void TestProjectHelper::Debug_LogMessage(FString&& InString)
 	UE_LOG(LogTest, Log, TEXT("%s"), *InString)
 }
 
-void TestProjectHelper::DeProjectScreenToWorld(const AMainController* PlayerControl, FVector& OutWorldPos, FVector& OutWorldDir)
+void TestProjectHelper::DeprojectScreenToWorld_Cursor(const AMainController* PlayerControl, FVector& OutWorldPos, FVector& OutWorldDir)
 {
 	FVector2D MousePosition;
 	PlayerControl->GetMousePosition(MousePosition.X, MousePosition.Y);    //获取鼠标位置
@@ -29,4 +29,17 @@ void TestProjectHelper::DeProjectScreenToWorld(const AMainController* PlayerCont
 
 	FMatrix const InvViewProjMatrix = ProjectionData.ComputeViewProjectionMatrix().InverseFast();     //获取投影矩阵的逆，这样就可以根据NDC空间得出在世界坐标系中的位置
 	FSceneView::DeprojectScreenToWorld(MousePosition, ProjectionData.GetConstrainedViewRect(), InvViewProjMatrix, OutWorldPos, OutWorldDir);   //其流程就是: 屏幕坐标-->NDC坐标-->世界坐标
+}
+
+void TestProjectHelper::DeprojectScreenToWorld_SpecifyPoint(const AMainController* PlayerControl, FVector2D ScreenPos, FVector& OutWorldPos, FVector& OutWorldDir)
+{
+	FVector2D ScreenSize;
+	PlayerControl->GetLocalPlayer()->ViewportClient->GetViewportSize(ScreenSize);   //获取屏幕尺寸
+
+	FSceneViewProjectionData ProjectionData;     //投影数据
+	FViewport* ViewPort = PlayerControl->GetLocalPlayer()->ViewportClient->Viewport;
+	PlayerControl->GetLocalPlayer()->GetProjectionData(ViewPort, eSSP_FULL, ProjectionData);      //获取当前的投影数据
+
+	FMatrix const InvViewProjMatrix = ProjectionData.ComputeViewProjectionMatrix().InverseFast();     //获取投影矩阵的逆，这样就可以根据NDC空间得出在世界坐标系中的位置
+	FSceneView::DeprojectScreenToWorld(ScreenSize * ScreenPos, ProjectionData.GetConstrainedViewRect(), InvViewProjMatrix, OutWorldPos, OutWorldDir);
 }
--- TestProject/Public/Common/TestProjectHelper.h
+++ TestProject/Public/Common/TestProjectHelper.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "MainController.h"
+#include "Gameplay/MainController.h"
 
 /**
  * 该项目通用工具类，主要是静态成员，如屏幕输出和日志
@@ -16,6 +16,11 @@ public:
 
 	static void Debug_LogMessage(FString&& InString);
 
-	/**根据屏幕空间计算其所在的世界空间*/
-	static void DeProjectScreenToWorld(const AMainController* PlayerContorl, FVector& OutWorldPos, FVector& OutWorldDir);
+	/**根据屏幕空间计算其所在的世界空间,只是鼠标位置*/
+	static void DeprojectScreenToWorld_Cursor(const AMainController* PlayerContorl, FVector& OutWorldPos, FVector& OutWorldDir);
+
+	/**根据指定屏幕上得点进行反投影
+	  *@ Param ScreenPos：屏幕上的位置，注意范围是 0-1 
+ 	  */
+	static void DeprojectScreenToWorld_SpecifyPoint(const AMainController* PlayerControl, FVector2D ScreenPos, FVector& OutWorldPos, FVector& OutWorldDir);
 };
