d85c00e3973d3921282c5e663ee8538791c7a35c
haiaimi <wanghai1997@outlook.com>
2018-06-25 18:21:33 +0800

    自定义触摸输入

---
+++ TestProject/Private/Gameplay/CustomTouchInput.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CustomTouchInput.h"
+#include "Gameplay/MainController.h"
+#include "GameFramework/PlayerInput.h"
+
+
+UCustomTouchInput::UCustomTouchInput(const FObjectInitializer& ObjectInitializer)
+{
+	PreTouched = false;
+	OwnerController = nullptr;
+}
+
+void UCustomTouchInput::UpdateInputStates(float DeltaTime)
+{
+	UpdateGameKeys(DeltaTime);
+	ProcessKeyStates(DeltaTime);
+}
+
+void UCustomTouchInput::UpdateGameKeys(float DeltaTime)
+{
+	if (!OwnerController && GetOuter())
+	{
+		OwnerController = CastChecked<AMainController>(GetOuter());
+	}
+
+	uint32 CurrentTouchState = 0;
+	if (OwnerController->PlayerInput->Touches[0].Z != 0)
+	{
+		CurrentTouchState |= (1 << 0);       //此时有一个触摸点
+	}
+
+	FVector Temp = OwnerController->PlayerInput->Touches[0];
+	FVector2D TouchPoint = FVector2D(Temp.X, Temp.Y);
+
+	if (CurrentTouchState)
+	{
+		if (PreTouched)
+			OnePointEvent[IE_Repeat].ExecuteIfBound(TouchPoint, 0.f);
+
+		if (!PreTouched)
+			OnePointEvent[IE_Pressed].ExecuteIfBound(TouchPoint, 0.f);
+	}
+	else
+	{
+		if (PreTouched)
+			OnePointEvent[IE_Released].ExecuteIfBound(TouchPoint, 0.f);
+	}
+	PreTouched = CurrentTouchState;
+}
+
+void UCustomTouchInput::ProcessKeyStates(float DeltaTime)
+{
+
+}
---
+++ TestProject/Public/Gameplay/CustomTouchInput.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CustomTouchInput.generated.h"
+
+DECLARE_DELEGATE_TwoParams(FOnePointInputEvent, const FVector2D&, float);
+/**
+ * 多点触控输入控制类
+ */
+UCLASS()
+class TESTPROJECT_API UCustomTouchInput : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	UCustomTouchInput(const FObjectInitializer& ObjectInitializer);
+
+	/**更新输入状态*/
+	void UpdateInputStates(float DeltaTime);
+	
+private:
+	/**更新游戏触摸输入状态*/
+	void UpdateGameKeys(float DeltaTime);
+
+	void ProcessKeyStates(float DeltaTime);
+
+public:
+	// 用于单指按下三个状态的代理，Pressed，Released，Repeat
+	FOnePointInputEvent OnePointEvent[3];     
+
+private:
+	class AMainController * OwnerController;
+
+	/**上一次更新是否点击了屏幕*/
+	bool PreTouched;
+
+	/**当前按键状态*/
+	EInputEvent CurKeyEvent; 
+};
