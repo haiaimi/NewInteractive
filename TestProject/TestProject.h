e82077f788c5c87503ca7eccec89858a02e8e66b
haiaimi <wanghai1997@outlook.com>
2018-06-26 18:15:45 +0800

    输入事件类型

--- TestProject/TestProject.h
+++ TestProject/TestProject.h
@@ -8,3 +8,15 @@ DECLARE_LOG_CATEGORY_EXTERN(LogTest, Log, All);
 
 #define COLLISION_LOCKERRAY     ECollisionChannel::ECC_GameTraceChannel1
 #define COLLISION_INVENTORYRAY  ECollisionChannel::ECC_GameTraceChannel2
+
+/**触摸输入的类型*/
+namespace EGameTouchKey
+{
+	enum Type
+	{
+		Tap,      //单击
+		Hold,	  //长按
+		Swipe,    //拖拽
+		Pinch,    //双指缩放
+	};
+}
