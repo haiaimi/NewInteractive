576c4f6a40dff561bec8fd8b617fd6cc2e174a98
haiaimi <wanghai1997@outlook.com>
2018-07-13 18:13:53 +0800

    添加拖拽操作

---
+++ TestProject/Private/CustomComponent/TCDragSwipeComponent.cpp
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TCDragSwipeComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "TouchHelper.h"
+
+
+// Sets default values for this component's properties
+UTCDragSwipeComponent::UTCDragSwipeComponent() :
+	TargetActor(nullptr),
+	CurTouchType(ECustomTouchType::Drag_1P)
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+// Called when the game starts
+void UTCDragSwipeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+// Called every frame
+void UTCDragSwipeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UTCDragSwipeComponent::OnDragPressed(const FVector2D& Point, FVector& LookDir)
+{
+	DetectActorOnPoint(Point, LookDir);
+}
+
+void UTCDragSwipeComponent::OnDragUpdate(const FVector2D& Point, FVector& LookDir)
+{
+	APlayerController* MyController = Cast<APlayerController>(GetOwner());
+	if (TargetActor)
+	{
+		if (MyController)
+		{
+			FVector WorldPos, WorldDir;
+			MyController->DeprojectScreenPositionToWorld(Point.X, Point.Y, WorldPos, WorldDir);
+
+			FVector ActorNewPos = FMath::LinePlaneIntersection(WorldPos, WorldPos + 1000.f*WorldDir, MovePlane);
+			TargetActor->SetActorLocation(ActorNewPos + TouchOffset);     //更新Actor的位置
+		}
+	}
+	else
+	{
+		//不存在目标Actor则继续检测
+		DetectActorOnPoint(Point, LookDir);
+	}
+}
+
+void UTCDragSwipeComponent::OnDragReleased(const FVector2D& Point)
+{
+	TargetActor = nullptr;
+}
+
+void UTCDragSwipeComponent::DetectActorOnPoint(const FVector2D& Point, FVector& LookDir)
+{
+	if (APlayerController* MyController = Cast<APlayerController>(GetOwner()))
+	{
+		FHitResult Result;
+		MyController->GetHitResultAtScreenPosition(Point, ECollisionChannel::ECC_WorldDynamic, false, Result);
+
+		if (Result.GetActor())
+		{
+			if (TouchHelper::IsTouchTypeContained(this, Result.GetActor(), ECustomTouchType::Drag_1P))
+			{
+				TargetActor = Result.GetActor();
+				TouchOffset = TargetActor->GetActorLocation() - Result.ImpactPoint;   //鼠标指针相对于物体的位置
+				MovePlane = FPlane(Result.ImpactPoint, -LookDir);   //获取鼠标与物体撞击点的平面
+			}
+		}
+	}
+}
+
