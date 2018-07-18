03b59f81c9e8dcd01a7bc1b4aaff244e2c72b22a
haiaimi <wanghai1997@outlook.com>
2018-07-18 18:32:19 +0800

    指定截椎体检测

--- TestProject/Private/Common/TouchHelper.cpp
+++ TestProject/Private/Common/TouchHelper.cpp
@@ -4,6 +4,9 @@
 #include "GameFramework/PlayerController.h"
 #include "GameFramework/PlayerInput.h"
 #include "TestProjectHelper.h"
+#include "ConvexVolume.h"
+#include "Kismet/GameplayStatics.h"
+#include "EngineUtils.h"
 
 void TouchHelper::GetTouchInputInfo(class APlayerController* InPlayer, TArray<FVector>& OutTouchPoint)
 {
@@ -28,4 +31,76 @@ bool TouchHelper::IsTouchTypeContained(UObject* TouchComponent, AActor* TargetAc
 	}
 
 	return Result;
+}
+
+void TouchHelper::GetAllActorsInOrIntersectFrustrum(class APlayerController* InPlayer, FVector2D StartPoint, FVector2D EndPoint, TArray<AActor>& OutActors)
+{
+	FConvexVolume Frustrum;   //检测的截椎体
+
+	TArray<FVector> WorldPos, WorldDir;
+	TArray<FVector2D> ScreenPos;   //屏幕上的位置
+	TArray<FPlane> FrustrumPlanes;   //截椎体的平面，不包含两端的
+	// 对应屏幕上4个点在世界空间的位置及方向，分别为左上，右上，右下，左下
+	ScreenPos.SetNum(4);
+	FrustrumPlanes.SetNum(4);
+	WorldPos.SetNum(4);
+	WorldDir.SetNum(4);
+	// 计算出屏幕上的4个点
+	const FVector2D TwoPointSub = EndPoint - StartPoint;
+	if (TwoPointSub.X > 0 && TwoPointSub.Y > 0)
+	{
+		ScreenPos[0] = StartPoint;
+		ScreenPos[1] = StartPoint + FVector2D(TwoPointSub.X, 0.f);
+		ScreenPos[2] = EndPoint;
+		ScreenPos[3] = StartPoint + FVector2D(0.f, TwoPointSub.Y);
+	}
+	else if (TwoPointSub.X > 0 && TwoPointSub.Y < 0)
+	{
+		ScreenPos[0] = EndPoint - FVector2D(TwoPointSub.X, 0.f);
+		ScreenPos[1] = EndPoint;
+		ScreenPos[2] = StartPoint - FVector2D(0.f, TwoPointSub.Y);
+		ScreenPos[3] = StartPoint;
+	}
+	else if (TwoPointSub.X < 0 && TwoPointSub.Y > 0)
+	{
+		ScreenPos[0] = EndPoint - FVector2D(0.f, TwoPointSub.Y);
+		ScreenPos[1] = StartPoint;
+		ScreenPos[2] = StartPoint + FVector2D(0.f, TwoPointSub.Y);
+		ScreenPos[3] = EndPoint;
+	}
+	else
+	{
+		ScreenPos[0] = EndPoint;
+		ScreenPos[1] = EndPoint - FVector2D(TwoPointSub.X, 0.f);
+		ScreenPos[2] = StartPoint;
+		ScreenPos[3] = EndPoint - FVector2D(0.f, TwoPointSub.Y);
+	}
+	// 根据屏幕上的点计算出在世界坐标系中的位置及方向
+	for (int32 i = 0; i < 4; i++)
+	{
+		UGameplayStatics::DeprojectScreenToWorld(InPlayer, ScreenPos[i], WorldPos[i], WorldDir[i]);
+	}
+	// 根据世界坐标系中的位置及方向求对应的平面
+	for (int32 j = 0; j < 4; j++)
+	{
+		const FVector WorldPointDir = WorldPos[(j + 1) % 4] - WorldPos[j];
+		const FVector PlaneNormal = FVector::CrossProduct(WorldDir[j], WorldPointDir);
+		FrustrumPlanes[j] = FPlane(WorldPos[j], PlaneNormal.GetSafeNormal());
+	}
+
+	Frustrum.Planes = FrustrumPlanes;    //填充截椎体里的平面信息
+	
+	// 遍历所有已经渲染的actor，判断是否被框选
+	for (TActorIterator<AActor> Iter(InPlayer->GetWorld()); Iter; ++Iter)
+	{
+		if ((*Iter)->WasRecentlyRendered())
+		{
+			bool bActorContained = false;
+			const FBox ActorBox = (*Iter)->GetComponentsBoundingBox();
+			bool bIntersected = Frustrum.IntersectBox(ActorBox.GetCenter(), ActorBox.GetExtent(), bActorContained);
+
+			if (bIntersected || bActorContained)
+				TestProjectHelper::Debug_ScreenMessage((*Iter)->GetName());
+		}
+	}
 }
--- TestProject/Public/Common/TouchHelper.h
+++ TestProject/Public/Common/TouchHelper.h
@@ -6,7 +6,7 @@
 #include "Interface/CustomTouchInterface.h"
 
 /**
- * 
+ *  触摸操作中使用的一些通用方法
  */
 class TESTPROJECT_API TouchHelper
 {
@@ -17,6 +17,15 @@ public:
 	  * @Param TouchComponent     触摸组件
 	  * @Param TargetActor        目标Actor
 	  * @Param ComponentTouchType 当前触摸组件的触摸模式
+	  * @Returns 如为true，则该目标Actor和触摸组件之间可以交互，false则不能
 	  */
 	static bool IsTouchTypeContained(class UObject* TouchComponent, class AActor* TargetActor, ECustomTouchType::Type ComponentTouchType);
+
+	/** 获取指定截椎体内所有的Actor，包括相交的Actor,这里会使用FConvex来进行检测
+	  * @Param InPlayer        请求调用的玩家
+	  * @Param StartPoint      框选开始的点（屏幕）
+	  * @Param EndPoint        框选结束得点（屏幕）
+	  * @Out OutActors  返回与指定截椎体相交或被包含的所有Actor
+	  */
+	static void GetAllActorsInOrIntersectFrustrum(class APlayerController* InPlayer, FVector2D StartPoint, FVector2D EndPoint, TArray<AActor>& OutActors);
 };
