2255f30be890b2d33a67a7150a0c7ccff64164a6
haiaimi <wanghai1997@outlook.com>
2018-08-20 18:31:15 +0800

    修改GameMode名

---
+++ WarSimulate/Private/Gameplay/WarSimulateGameMode.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Gameplay/WarSimulateGameMode.h"
+#include "UI/WarSimulateHUD.h"
+#include "Gameplay/PlatformController.h"
+#include "Gameplay/GroundSpectatorPawn.h"
+
+AWarSimulateGameMode::AWarSimulateGameMode(const FObjectInitializer& Initializer)
+{
+	PlayerControllerClass = APlatformController::StaticClass();
+	HUDClass = AWarSimulateHUD::StaticClass();
+	DefaultPawnClass = AGroundSpectatorPawn::StaticClass();
+	SpectatorClass = AGroundSpectatorPawn::StaticClass();
+}
+
+
