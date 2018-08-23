fad5aec4f4dd2b21de45ee187fef05919c26046d
haiaimi <wanghai1997@outlook.com>
2018-08-23 18:43:57 +0800

    添加显控UI

---
+++ WarSimulate/Private/UI/Widgets/SVisualControlWidget.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "SlateBasics.h"
+#include "SlateExtras.h"
+#include "Widgets/SCompoundWidget.h"
+
+class APlatformController;
+/**
+ * 
+ */
+class SVisualControlWidget : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SVisualControlWidget)
+	{}
+	SLATE_ARGUMENT(TWeakObjectPtr<APlatformController>, OwnerController)
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+
+private:
+	TSharedPtr<class SImage> VisualControlImage;
+
+	/**显控材质*/
+	class UMaterialInstanceDynamic* VisualControlMaterial;
+
+	TWeakObjectPtr<APlatformController> OwnerController;
+};
